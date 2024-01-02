1bec450c3ceda31d8c7e01f3a50b6d7a8846844a
Vitalii Maksymenko <maxymenkovitaly@gmail.com>
2024-01-02 22:37:50 +0100

    compute usernames

--- script.js
+++ script.js
@@ -74,11 +74,24 @@ const displayMovements = function (movements) {
   </div>
   `;
 
-    containerMovements.insertAdjacentHTML('beforeend', html);
+    containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 
 displayMovements(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
